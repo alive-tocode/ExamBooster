b4faeb6311d169ab2c765360b12aac22e67e259a
Raunak Raj <raunakraj74730@gmail.com>
2021-08-08 16:34:45 +0530

    Main Page Design

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+    document.getElementById("mySidepanel").style.height = "50vh";
+
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
