c1bf3ba357152f6365fdbd5d46954e658e499a05
Raunak Raj <raunakraj74730@gmail.com>
2021-08-09 18:13:23 +0530

    Index page complete design

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+
+  var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
