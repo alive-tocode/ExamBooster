2ae2537a1578b1b9f14b567607ee2f5a857b4061
Parth Chaudhary <parth20102001@gmail.com>
2021-08-18 17:33:15 +0530

    pop up problem resolved

--- public/js/main.js
+++ public/js/main.js
@@ -24,9 +24,9 @@ function typeWriter() {
 
 
 
-document.getElementById("open-popup-btn2").addEventListener("click", function() {
-    document.getElementsByClassName("popup")[0].classList.add("active");
-});
+// document.getElementById("open-popup-btn2").addEventListener("click", function() {
+//     document.getElementsByClassName("popup")[0].classList.add("active");
+// });
 document.getElementById("open-popup-btn3").addEventListener("click", function() {
     document.getElementsByClassName("popup")[0].classList.add("active");
 });
